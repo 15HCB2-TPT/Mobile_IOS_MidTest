166893cbd5fda903c57f162789486dc4bc0cc06a
phanhaugiang <thienthientan.tin@gmail.com>
2017-04-13 14:02:35 +0700

    Done Region

--- QLQuanAn/QLQuanAn/QuanLyQuanAn/QuanLyQuanAn/Helper/UIPassingData.swift
+++ QLQuanAn/QLQuanAn/QuanLyQuanAn/QuanLyQuanAn/Helper/UIPassingData.swift
@@ -10,19 +10,31 @@ import Foundation
 import UIKit
 
 protocol UIPassingProtocol {
-    func uiPassedData(data: Any?)
+    func uiPassedData(data: Any?, identity: Int)
 }
 
 extension UIPassingProtocol {
-    func sendData(data: Any?) {
-        self.uiPassedData(data: data)
+    func sendData(data: Any?, identity: Int) {
+        self.uiPassedData(data: data, identity: identity)
     }
 }
 
 class UIPassingData {
-    static func sendData(view: UIPassingProtocol, data: Any?, delay: Double = 0.25){
+    static func sendData(view: UIPassingProtocol, data: Any?, identity: Int = 0, delay: Double = 0.25){
         DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
-            view.sendData(data: data)
+            view.sendData(data: data, identity: identity)
+        })
+    }
+    
+    static func sendData(fromController: UIViewController, storyboard: String, controller: String, data: Any?, identity: Int = 0, delay: Double = 0.25) {
+        let s = UIStoryboard(name: storyboard, bundle: nil)
+        let c = s.instantiateViewController(withIdentifier: controller) 
+        fromController.present(c, animated: true, completion: { () -> Void in
+            do {
+                try UIPassingData.sendData(view: c as! UIPassingProtocol, data: data, identity: identity, delay: delay)
+            } catch _ {
+                print("Can't pass data to \(c)")
+            }
         })
     }
 }

ee6a565be01bf24d9fbf9fc3003b715a84c52ae9
Tú Phạm <chungtu3108@yahoo.com.vn>
2017-04-20 16:09:46 +0700

    update food

--- QLQuanAn/QLQuanAn/QuanLyQuanAn/QuanLyQuanAn/Controller/Food/Food_ViewController.swift
+++ QLQuanAn/QLQuanAn/QuanLyQuanAn/QuanLyQuanAn/Controller/Food/Food_ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Food_ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class Food_ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate {
     @IBOutlet weak var txtTen: UITextField!
     @IBOutlet weak var stcTimNangCao: UIStackView!
     @IBOutlet weak var heightTimNangCao: NSLayoutConstraint!
@@ -19,7 +19,12 @@ class Food_ViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var swtTimNC: UISwitch!
     @IBOutlet weak var tblMonAn: UITableView!
     
-    var foods = Database.select(entityName: "Food") as! [Food]
+    var foods = Database.select(entityName: "Food", predicater: nil, sorter: [NSSortDescriptor(key: "name", ascending: true)]) as! [Food]
+    var foodType = Database.select(entityName: "FoodType", predicater: nil, sorter: [NSSortDescriptor(key: "nametype", ascending: true)]) as! [FoodType]
+    
+    var pckLoaiMonAn: UIPickerView!
+    var tempString: String! = ""
+    var tempRow: Int! = 0
     
     @IBAction func swtTimNC_ValueChanged(_ sender: Any) {
         if(stcTimNangCao.isHidden == true) {
@@ -30,6 +35,20 @@ class Food_ViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
         stcTimNangCao.isHidden = !stcTimNangCao.isHidden
     }
+    
+    @IBAction func btnTim_TouchUpInside(_ sender: Any) {
+        let conditionName = NSPredicate(format: "name CONTAINS[cd] %@", txtTen.text!)
+        let conditionType = NSPredicate(format: "food_type.nametype == %@", txtLoaiMonAn.text!)
+        let conditionMoney = NSPredicate(format: "(%f <= money) AND (money <= %f)", Double.init(txtTu.text!)!, Double.init(txtDen.text!)!)
+        var conditions: [NSPredicate] = []
+        if(txtTen.text != "") { conditions.append(conditionName) }
+        if(swtTimNC.isOn) {
+            if(txtLoaiMonAn.text != "") { conditions.append(conditionType) }
+            if(txtTu.text != "" && txtDen.text != "") { conditions.append(conditionMoney) }
+        }
+        foods = Database.select(entityName: "Food", predicater: NSCompoundPredicate(andPredicateWithSubpredicates: conditions), sorter: [NSSortDescriptor(key: "name", ascending: true)]) as! [Food]
+        tblMonAn.reloadData()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +56,16 @@ class Food_ViewController: UIViewController, UITableViewDelegate, UITableViewDat
         txtTen.inputAccessoryView = addDoneButton()
         txtTu.inputAccessoryView = addDoneButton()
         txtDen.inputAccessoryView = addDoneButton()
+        
+        tempString = foodType[0].nametype
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: self.view.frame.size.height/6, width: self.view.frame.size.width, height: 40.0))
+        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        let btnHuy = UIBarButtonItem(title: "Huỷ", style: .plain, target: self, action: #selector(Food_ViewController.btnHuy_Touch))
+        let btnChon = UIBarButtonItem(title: "Chọn", style: .plain, target: self, action: #selector(Food_ViewController.btnChon_Touch))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
+        toolBar.setItems([btnHuy,flexSpace,flexSpace,btnChon], animated: true)
+        txtLoaiMonAn.inputAccessoryView = toolBar
+        
         tblMonAn.reloadData()
         
         // Uncomment the following line to preserve selection between presentations
@@ -51,16 +80,49 @@ class Food_ViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func txtLoaiMonAn_EditingDidBegin(_ sender: Any) {
+        pckLoaiMonAn = UIPickerView()
+        pckLoaiMonAn.delegate = self
+        pckLoaiMonAn.selectRow(tempRow, inComponent: 0, animated: true)
+        txtLoaiMonAn.inputView = pckLoaiMonAn
+    }
+    
+    func btnHuy_Touch(sender: UIBarButtonItem) {
+        txtLoaiMonAn.resignFirstResponder()
+    }
+    
+    func btnChon_Touch(sender: UIBarButtonItem) {
+        txtLoaiMonAn.text = tempString
+        txtLoaiMonAn.resignFirstResponder()
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return foodType.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return foodType[row].nametype!
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tempRow = row
+        tempString = foodType[row].nametype
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return foods.count
     }
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellFood", for: indexPath) as! Food_TableViewCell
         cell.imgHinh.image = UIImage(data: foods[indexPath.row].image! as Data)
         cell.lblTenMonAn.text = foods[indexPath.row].name!
-        cell.lblGia.text = "\(foods[indexPath.row].money)"
+        if(foods[indexPath.row].is_use == true) {
+            cell.lblGia.text = "\(foods[indexPath.row].money)"
+        }
+        else {
+            cell.lblGia.text = "\(foods[indexPath.row].money)" + " - Ngừng kinh doanh"
+        }
         return cell
     }
  
